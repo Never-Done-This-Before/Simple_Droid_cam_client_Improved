bddeddd479166db2d1ccf99f2f9ad7b7917ccae1
Never-Done-This-Before <130860022+Never-Done-This-Before@users.noreply.github.com>
2023-04-16 05:47:45 +1000

    Delete simple-droidcam-client-0.8.0_fully_working/build directory

--- simple-droidcam-client-0.8.0_fully_working/build/db/simple_droidcam_client_db_resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_simple_droidcam_client_db_H__
-#define __RESOURCE_simple_droidcam_client_db_H__
-
-#include <gio/gio.h>
-
-G_GNUC_INTERNAL GResource *simple_droidcam_client_db_get_resource (void);
-#endif
